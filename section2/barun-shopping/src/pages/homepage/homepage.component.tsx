110cbcb02818cbf4976fa2373fde454d58b40fc3
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2022-07-08 15:07:55 +0545

    broken homepage into smaller comps

---
+++ section2/barun-shopping/src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,13 @@
+import  DirectoryMenu  from './../../components/directory-menu/directory-menu.component';
+import React from 'react';
+
+import './homepage.styles.scss'
+
+const Homepage = () =>{
+    return <div className="homepage">
+        <DirectoryMenu     />
+    </div>
+}
+
+
+export default Homepage
